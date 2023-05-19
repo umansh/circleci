b05e34add7c2a393b712f478704703a579f886f9
umansh <umangshiroya543@gmail.com>
2023-05-19 14:29:10 +0200

    add file

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
         name = "world"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name, "world")
+        self.assertEqual(upper_name, "WORLD")
 
 
 if __name__ == '__main__':
